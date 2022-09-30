099fa47e5f7c78d8fd632c67f0b26d737c44bef9
Evgeniy <>
2022-09-30 20:41:34 +0300

    add task6 minor fix

--- Seminar1HomeWork/task6.py
+++ Seminar1HomeWork/task6.py
@@ -5,7 +5,7 @@ inputDigit = int(input('Введите целое число для провер
 
 if inputDigit == 0:
     print('Анализ не имеет смысла, т.к. введен 0')
-elif (inputDigit % 5 == 0 or inputDigit % 10 == 0 or inputDigit % 15 == 0) and not inputDigit % 30 == 0:
+elif (inputDigit % 5 == 0 and inputDigit % 10 == 0 or inputDigit % 15 == 0) and not inputDigit % 30 == 0:
     print(f'Число {inputDigit} кратно 5,10 или 15, но не кратно 30')
 else:
     print(f'Число {inputDigit} не кратно 5,10 или 15 или кратно 30')
