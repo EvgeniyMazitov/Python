d9acb96f8278ed662b84c82b4ca4f39dc8e6ea99
Evgeniy <>
2022-10-01 20:34:53 +0300

    add task2

--- Seminar2HomeWork/Task2.py
+++ Seminar2HomeWork/Task2.py
@@ -2,6 +2,7 @@
 # Пример:
 # пусть N = 4, тогда (1, 1*2, 1*2*3, 1*2*3*4)
 
+# Ввод и проверка числа
 def EnterInt(question):
     number = 0
     while True:
@@ -12,31 +13,33 @@ def EnterInt(question):
             print("Введеное значение не допустимо, попробуйте еще раз")
     return number
 
-# # На всякий случай вариант с расчетом всех значений факториала от 1 до N и вывод результата умножения, а не строки со знаками
-
-# def factorial(number):
-#     if number == 1:
-#         return 1
-#     else:
-#         return number * factorial(number-1)
+# Расчет факториала
 
 
-# numberN = EnterInt("Введите число N: ")
-# list = []
-# for i in range(1, numberN+1):
-#     list.append(factorial(i))
-# print(list)
+def factorial(number):
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number-1)
 
 
-# Вывод как в задании
 numberN = EnterInt("Введите число N: ")
+
 list = []
+
+# Формирование списка значений через функцию факториала
+for i in range(1, numberN+1):
+    list.append(factorial(i))
+
+# Формирование списка с перечнем множителей
+result = []
 for i in range(1, numberN+1):
     inputString = ""
     for j in range(1, i+1):
         inputString += str(j)
         if j < i:
             inputString += "*"
-    list.append(inputString)
+    result.append(inputString)
 
+print(result)
 print(list)
