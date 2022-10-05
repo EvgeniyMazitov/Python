017cf24180f6664afd943fbc6cfac1a24b3e7f96
Evgeniy <>
2022-10-05 16:05:51 +0300

    add task 1,2

--- Seminar3HomeWork/Task1.py
+++ Seminar3HomeWork/Task1.py
@@ -3,3 +3,29 @@
 
 # Пример:
 # [2, 3, 5, 9, 3] => на нечётных позициях элементы 3 и 9, ответ: 12
+
+
+def EnterIntNumber(question):
+    number = 0
+    while True:
+        try:
+            number = int(input(question))
+            break
+        except:
+            print("Введеное значение не допустимо, попробуйте еще раз")
+    return number
+
+
+list = []
+LenList = EnterIntNumber("Введите количество элементов в списке : ")
+
+for i in range(0, LenList):
+    list.append(EnterIntNumber(
+        f'Введите значение {i+1}-го элемента списка : '))
+
+print(f'Анализируемый список: {list}')
+
+sum = 0
+for i in range(1, len(list)-1, 2):
+    sum += list[i]
+print(f'Сумма элементов на нечетных позициях списка = {sum}')
--- Seminar3HomeWork/Task2.py
+++ Seminar3HomeWork/Task2.py
@@ -4,3 +4,34 @@
 # Пример:
 # [2, 3, 4, 5, 6] => [12, 15, 16]
 # [2, 3, 5, 6] => [12, 15]
+
+def EnterIntNumber(question):
+    number = 0
+    while True:
+        try:
+            number = int(input(question))
+            break
+        except:
+            print("Введеное значение не допустимо, попробуйте еще раз")
+    return number
+
+
+def MultiElements(list):
+    resultList = []
+    for i in range(0, round(len(list)/2)):
+        resultList.append(list[i]*list[-i-1])
+    if len(list) % 2 == 1:
+        resultList.append(list[round(len(list)/2)]**2)
+    return resultList
+
+
+list = []
+resultList = []
+LenList = EnterIntNumber("Введите количество элементов в списке : ")
+
+for i in range(0, LenList):
+    list.append(EnterIntNumber(
+        f'Введите значение {i+1}-го элемента списка : '))
+
+print(f'Анализируемый список: {list}')
+print(f'Сумма произведений пар чисел списка = {MultiElements(list)}')
