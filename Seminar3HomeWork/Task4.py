14a1cd2e7d599a9d8b6fb504f163e990c982f9e1
Evgeniy <>
2022-10-05 20:11:43 +0300

    add task 4

--- Seminar3HomeWork/Task4.py
+++ Seminar3HomeWork/Task4.py
@@ -5,3 +5,36 @@
 # 45 => 101101
 # 3 => 11
 # 2 => 10
+
+def EnterIntNumber(question):
+    number = 0
+    while True:
+        try:
+            number = int(input(question))
+            break
+        except:
+            print("Введеное значение не допустимо, попробуйте еще раз")
+    return number
+
+
+def StringRevers(str):
+    ReversStr = ""
+    for i in range(len(str)-1, -1, -1):
+        ReversStr += str[i]
+    return ReversStr
+
+
+numberD = EnterIntNumber(
+    "Введите десятичное целое положительное число для перевода в двоичное : ")
+
+buffer = numberD
+resultString = ""
+
+while buffer != 0:
+    resultString += str(buffer % 2)
+    buffer = int(buffer/2)
+print(resultString)
+ReversStr = StringRevers(resultString)
+
+print(
+    f'Десятичное число {numberD} по модулю в двоичной системе исчисления = {ReversStr}')
