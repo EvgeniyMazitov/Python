60f6226d0cabaed3a95b9712a823302e59b2d811
Evgeniy <>
2022-10-12 18:33:32 +0300

    add some interface

--- Seminar5HomeWork/Task2.py
+++ Seminar5HomeWork/Task2.py
@@ -5,11 +5,74 @@
 # 2. Подумайте как наделить бота 'интеллектом'
 # Оба задания обязательны
 
+
 import tkinter as tk
+
+
+def StartGame():
+    label123 = tk.Label(mainWindow, text=f'Начата новая игра {count}',
+                        font=('Arial', 15, 'bold'))
+    buttom_GameStart['state'] = tk.DISABLED
+
+    label123.pack()
+
+
+def NextStep():
+    global count
+    count += 1
+    buttom_NextStep['text'] = f'Сделать ход {count}'
+    valueEntry = entryValue.get()
+    if valueEntry:
+        print(valueEntry)
+    else:
+        print('Введите количество конфет и нажмите еще раз')
+
+
+count = 1
 mainWindow = tk.Tk()
 photo = tk.PhotoImage(file='Seminar5HomeWork\IcoGame.png')
 mainWindow.iconphoto(False, photo)
 mainWindow.title('Игра с конфетами')
 mainWindow.geometry("500x600+300+100")
 mainWindow.resizable(False, False)
+
+
+radioOption = tk.StringVar(value='PvP')
+
+label_GameType = tk.Label(
+    mainWindow, text='Выберите тип игры', font=('Arial', 15, 'bold'))
+label_GameType.pack()
+
+
+radio_GameType_PvP = tk.Radiobutton(mainWindow,
+                                    text='PvP', value='PvP', textvariable=radioOption)
+radio_GameType_PvP.pack()
+
+radio_GameType_PvBot = tk.Radiobutton(mainWindow,
+                                      text='PvBot', value='PvBot', textvariable=radioOption)
+radio_GameType_PvBot.pack()
+
+
+label_GameStart = tk.Label(
+    mainWindow, text=f'Нажмите кнопку "Начать игру":', font=('Arial', 15, 'bold'))
+label_GameStart.pack()
+
+buttom_GameStart = tk.Button(mainWindow, text='Начать игру', command=StartGame)
+buttom_GameStart.pack()
+
+label_PlayerName = tk.Label(
+    mainWindow, text=f'Ход игрока: ', font=('Arial', 15, 'bold'))
+label_PlayerName.pack()
+
+label_EntryValue = tk.Label(
+    mainWindow, text='Введите количество конфет', font=('Arial', 15, 'bold'))
+label_EntryValue.pack()
+
+entryValue = tk.Entry(mainWindow, text='1')
+entryValue.pack()
+
+buttom_NextStep = tk.Button(
+    mainWindow, text=f'Сделать ход {count}', command=NextStep)
+buttom_NextStep.pack()
+
 mainWindow.mainloop()
