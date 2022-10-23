b36427f9afbefa13dbdabb26792fcbfdee3b5c5d
Evgeniy <>
2022-10-23 15:59:59 +0300

    Add additional functions

---
+++ Seminar6/Task5.py
@@ -0,0 +1,51 @@
+# from secrets import choice
+
+
+# phone_book = [['Игорь Моисеев', '89093152088', 'Рабочий'], ['Алина Проценко', '89685129970', 'Домашний'], ['Валентина Гаврилова',
+#                                                                                                            '89172140089', 'Домашний'], ['Сергей Копреев', '89014506622', 'Домашний'], ['Наталья Иванова', '89075008203', 'Рабочий']]
+
+# choise = 2
+# value = 'Рабочий'
+# search_result = []
+
+# print(" ".join(phone_book))
+# # for i in phone_book:
+# #     if value in i[choise]:
+# #         search_result.append(i)
+
+# for id, item in enumerate(phone_book):
+#     if value in item[int(choise)]:
+#         search_result.append(id)
+#         search_result.append(item)
+
+# # print(search_result)
+
+# for i in range(0, len(search_result), 2):
+#     print(search_result[i], *search_result[i+1])
+
+
+# for id, item in enumerate(phone_book):
+# # for i in phone_book:
+# #     if value in i[int(choise)]:
+# #         search_result.append(i)
+# # return search_result
+#     if value in item[int(choise)]:
+#         search_result.append(id)
+#         search_result.append(item)
+# return search_result
+
+
+# def search_contact(choise, value):
+#     global phone_book
+#     for i in phone_book:
+#         if value in i[choise]:
+#             print(phone_book[i])
+
+# list_of_list = []
+# for i in range(5):
+#     new = []
+#     for k in range(5):
+#         new.append(random.randint(0,10))
+
+# for index, item in list_of_list
+# ';'.join(item)
